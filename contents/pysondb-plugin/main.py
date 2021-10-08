74d44925098fe0ce6bf2cd1258336d7bae1db815
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-10-08 14:30:34 +0530

    Added contents for pysondb plugin

---
+++ contents/pysondb-plugin/main.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+class Plugin:
+
+    def __init__(self) -> None:
+        pass
+
+    def plugin_parser(self, subparser: argparse._SubParsersAction) -> None:
+        """
+        The subparser is passed in as the only argument, and you can add the arguments for you command
+        here
+        """
+        pass
+
+    def action(self, args: argparse.Namespace) -> None:
+        """
+            The action methods is called when the command is executed by the user and the entire
+             args namespace is passed in.
+        """
+        pass
